12f1f21e0a8107b94d59c24ea5dbf0191d6f9229
Gang ZHANG <gangz@emergentdesign.cn>
2020-02-22 15:26:58 +0800

    rename python to python3, for future extension of python

---
+++ src/main/java/depends/extractor/python3/PythonHandlerContext.java
@@ -0,0 +1,14 @@
+package depends.extractor.python3;
+
+import depends.entity.repo.EntityRepo;
+import depends.extractor.HandlerContext;
+import depends.relations.Inferer;
+
+public class PythonHandlerContext extends HandlerContext {
+
+	public PythonHandlerContext(EntityRepo entityRepo,Inferer inferer) {
+		super(entityRepo,inferer);
+	}
+
+
+}
