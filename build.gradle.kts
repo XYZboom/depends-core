a5a50a31086b0d4d86ece6cf4d0672b150c4e9c8
XYZboom <xyzboom@qq.com>
2024-05-10 11:53:14 +0800

    <build> use jitpack for repo manage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("maven-publish")
+    application
 }
 
 group = "cn.emergentdesign.se"
@@ -11,53 +11,16 @@ java {
     targetCompatibility = JavaVersion.VERSION_17
 }
 
-publishing {
-    repositories {
-        maven {
-            url = uri("http://47.115.213.131:8080/repository/alex-snapshots/")
-            credentials {
-                username = properties["maven-username"].toString()
-                password = properties["maven-password"].toString()
-            }
-            isAllowInsecureProtocol = true
-        }
-    }
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "cn.emergentdesign.se"
-            artifactId = "depends-core"
-            version = "0.9.8-SNAPSHOT"
-
-            from(components["java"])
-        }
-        create<MavenPublication>("maven-source") {
-            groupId = "cn.emergentdesign.se"
-            artifactId = "depends-core"
-            version = "0.9.8-SNAPSHOT"
-
-            // 配置要上传的源码
-            artifact(tasks.register<Jar>("sourcesJar") {
-                from(sourceSets.main.get().allSource)
-                archiveClassifier.set("sources")
-            }) {
-                classifier = "sources"
-            }
-        }
-    }
-}
-
 repositories {
-    maven {
-        url = uri("http://47.115.213.131:8080/repository/alex-release/")
-        isAllowInsecureProtocol = true
-    }
+    mavenLocal()
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
     implementation("org.jetbrains:annotations:24.0.1")
     implementation("org.antlr:antlr4-runtime:4.13.1")
-    implementation("cn.emergentdesign.se:utils:0.1.1")
+    implementation("com.github.multilang-depends:utils:04855aebf3")
     implementation("javax.xml.bind:jaxb-api:2.3.1")
     implementation("org.apache.poi:poi:5.2.2")
     implementation("org.apache.poi:poi-ooxml:5.2.4")
@@ -65,9 +28,15 @@ dependencies {
     implementation("org.codehaus.plexus:plexus-utils:3.5.1")
     implementation("net.sf.ehcache:ehcache-core:2.6.11")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.1")
+    runtimeOnly("com.github.XYZboom:depends-java:1.0.0-alpha1")
+    runtimeOnly("com.github.XYZboom:depends-kotlin:v1.0.0-alpha0")
     testImplementation("junit:junit:4.13.2")
 }
 
 tasks.getByName<Test>("test") {
     useJUnit()
+}
+
+application {
+    mainClass.set("depends.Main")
 }
